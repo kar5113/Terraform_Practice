88279510cdc5ba8470a1d2b5b429cf429e252d1f
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-22 17:58:43 +0530

    dhdhd

--- Roboshop-dev-infra/20-bastion/bastion.tf
+++ Roboshop-dev-infra/20-bastion/bastion.tf
@@ -3,8 +3,20 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   subnet_id = local.public_subnet_ids[0]
   vpc_security_group_ids = [local.bastion_sg_id]  
-
+  root_block_device {
+    volume = 50
+  }
 # Install terraform, ansible , git here
+user_data = <<-EOF
+              #!/bin/bash
+              growpart /dev/nvme0n1 4
+              lvextend -L +30G /dev/mapper/RootVG-varVol
+              xfs_growfs /var
+              sudo yum install -y yum-utils
+              sudo yum install -y terraform
+              sudo yum install -y git
+              sudo yum install -y ansible
+          EOF
 
   tags = merge(
         local.common_tags,{
@@ -12,27 +24,3 @@ resource "aws_instance" "example" {
         }
     )
 }
-# already a sg is created for bastion in 10-SG module
-# resource "aws_security_group" "allow_all" {
-#   name = "allow-all"
-#     description = "Allow all inbound traffic"
-
-#     egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-
-#   }
-#   ingress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-
-#   }
-
-#   tags={
-#     Name = "allow-all"
-#   }
-# }
--- Roboshop-dev-infra/20-bastion/datasource.tf
+++ Roboshop-dev-infra/20-bastion/datasource.tf
@@ -9,13 +9,13 @@ data "aws_ami" "ami" {
 }
 
 data "aws_ssm_parameter" "bastion_sg" {
-  name = "${var.project}/${var.environment}/bastion-sg_sg_id"
+  name = "/${var.project}/${var.environment}/bastion-sg_sg_id"
 }
 
 data "aws_ssm_parameter" "vpc_id" {
-  name = "${var.project}/${var.environment}/vpc_id"
+  name = "/${var.project}/${var.environment}/vpc_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "${var.project}/${var.environment}/public_subnet_ids"
+  name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
