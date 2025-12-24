8afa0f86fa2d3956398441cd8908585c6f0ce114
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-24 14:57:37 +0530

    sfh

--- Roboshop-dev-infra/60-catalogue/main.tf
+++ Roboshop-dev-infra/60-catalogue/main.tf
@@ -59,95 +59,32 @@ resource "aws_ec2_instance_state" "stop_catalogue" {
 
 # create an ami using the stopped instance
 resource "aws_ami_from_instance" "catalogue_ami" {
-  name               = "catalogue-ami-${var.environment}-${timestamp()}"
+  name               = "catalogue-ami-${var.environment}-${date("YYYYMMDDHHMM")}"
   source_instance_id = aws_instance.catalogue.id
 }
 
 # create a launch template using that ami
 resource "aws_launch_template" "catalogue_launch_template" {
   name = "catalogue-${var.environment}"
-
-  block_device_mappings {
-    device_name = "/dev/sdf"
-
-    ebs {
-      volume_size = 20
-    }
-  }
-
-  capacity_reservation_specification {
-    capacity_reservation_preference = "open"
-  }
-
-  cpu_options {
-    core_count       = 4
-    threads_per_core = 2
-  }
-
-  credit_specification {
-    cpu_credits = "standard"
-  }
-
-  disable_api_stop        = true
-  disable_api_termination = true
-
-  ebs_optimized = true
-
-  iam_instance_profile {
-    name = "test"
-  }
-
-  image_id = "ami-test"
-
-  instance_initiated_shutdown_behavior = "terminate"
-
-  instance_market_options {
-    market_type = "spot"
-  }
-
-  instance_type = "t2.micro"
-
-  kernel_id = "test"
-
-  key_name = "test"
-
-  license_specification {
-    license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
-  }
-
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "enabled"
-  }
-
-  monitoring {
-    enabled = true
-  }
-
-  network_interfaces {
-    associate_public_ip_address = true
-  }
-
+  image_id = aws_ami_from_instance
+  instance_type = "t3.micro"
   placement {
-    availability_zone = "us-west-2a"
+    availability_zone = local.private_subnet_id
   }
-
-  ram_disk_id = "test"
-
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [local.catalogue_sg_id]
 
   tag_specifications {
-    resource_type = "instance"
+    service = "catalogue-${var.environment}"
 
-    tags = {
-      Name = "test"
-    }
+    tags =  merge(
+        local.common_tags,{
+            Name= "${local.common_name_suffix}-catalogue-launch-template"
+        }
+    )
   }
-
-  user_data = filebase64("${path.module}/example.sh")
 }
+
+
 
 
 # stop the instance and create an ami from it.
