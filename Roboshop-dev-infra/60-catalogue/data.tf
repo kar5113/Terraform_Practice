5fe926ab9ce8d746156412f24b57e9d3d3b56328
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-24 14:37:04 +0530

    sfh

--- Roboshop-dev-infra/60-catalogue/data.tf
+++ Roboshop-dev-infra/60-catalogue/data.tf
@@ -8,14 +8,14 @@ data "aws_ami" "ami" {
   }
 }
 
-# data "aws_ssm_parameter" "catalogue_sg" {
-#   name = "/${var.project}/${var.environment}/catalogue_sg_id"
-# }
+data "aws_ssm_parameter" "catalogue_sg" {
+  name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
 
-# data "aws_ssm_parameter" "vpc_id" {
-#   name = "/${var.project}/${var.environment}/vpc_id"
-# }
+data "aws_ssm_parameter" "vpc_id" {
+  name = "/${var.project}/${var.environment}/vpc_id"
+}
 
-# data "aws_ssm_parameter" "private_subnet_ids" {
-#   name = "/${var.project}/${var.environment}/private_subnet_ids"
-# }
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project}/${var.environment}/private_subnet_ids"
+}
