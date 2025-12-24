c8bd9ec3603541a2f2c62f36c17ce96ee3778bf4
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-24 18:31:40 +0530

    sfh

--- Roboshop-dev-infra/20-bastion/configure.sh
+++ Roboshop-dev-infra/20-bastion/configure.sh
@@ -15,6 +15,8 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
+sudo yum install awscli -y
+
 
 # Installing Ansible
 sudo dnf install ansible -y
