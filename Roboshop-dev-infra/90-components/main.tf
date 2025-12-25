f4c933fe1afb569a24f61b19711f85ba814621a6
karthikeyareddyankireddy <karthikeya.ankireddy@rabobank.nl>
2025-12-25 16:48:32 +0530

    ffd

--- Roboshop-dev-infra/90-components/main.tf
+++ Roboshop-dev-infra/90-components/main.tf
@@ -1,13 +1,9 @@
 module "components" {
    for_each = var.component
-    source = "../Roboshop-component"
-
+    
+    source = "../Roboshop-component" 
+    
     component = each.key
-    rule_priority = each.value
-
-
-
-
-
   
+    rule_priority = each.value
 }
